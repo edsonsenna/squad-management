4be5019da5eefbd3c7e31796bad13c20a853b9ab
edsondesennajunior <edsonsjr62@gmail.com>
2020-10-22 08:39:48 -0300

    Creating scaffold component

---
+++ src/components/scaffold/Scaffold.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+
+import './styles.css';
+
+const Scaffold = () => {
+    return (
+        <div className="root">
+            <header>
+                <div className="company-info">
+                    <img className="company-logo" src={Logo} alt="Company Logo"></img>
+                    <span className="company-name">Squad Management Tool</span>
+                </div>
+                <div className="user-info">
+                    <span>John Doe</span>
+                    <div className="user-logo">
+                        <span>JD</span>
+                    </div>
+                </div>
+            </header> 
+            <div className="content">
+                Scaffold rocks!
+            </div>
+            <footer>2020 - All rights reserved.</footer>
+        </div>
+    );
+}
+
+export default Scaffold;
